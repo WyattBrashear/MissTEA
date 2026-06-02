462786e449f274a1d70400281f34c898a8a8e915
wyattbrashear <bfamily610@icloud.com>
2026-06-02 13:33:58 -0400

    EVERYTHING WORKSSSSS

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include <Arduino.h>
+// Simple test script to figure out if everything is working
+int buttonpin = 2;
 void setup() {
 // write your initialization code here
+    pinMode(buttonpin, INPUT_PULLUP);
+    Serial.begin(9600);
 }
 
 void loop() {
 // write your code here
+    Serial.println(digitalRead(buttonpin));
 }
