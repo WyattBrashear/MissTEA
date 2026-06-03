1c6a48e478c55297b26e0a2bf7a7967761ee4c30
wyattbrashear <bfamily610@icloud.com>
2026-06-02 21:33:44 -0400

    add Devlog 3 work

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,28 @@
 #include <Arduino.h>
 #include <WiFiS3.h>
 #include <arduino_secrets.h>
-// Simple test script to figure out if WiFi
-
+// Simple test script to figure out if WiFi works
+char ssid[] = SECRET_SSID;
+char pass[] = SECRET_PASS;
+int startButton = 2;
+int tempPot = A0;
+int timePot = A1;
+int speedPot = A2;
 void setup() {
 // write your initialization code here
     Serial.begin(9600);
+    pinMode(startButton, INPUT_PULLUP);
 }
 
 void loop() {
 // write your code here
-    Serial.println("HELLO WORLD");
+    Serial.print("Status: ");
+    Serial.print(analogRead(tempPot));
+    Serial.print(", ");
+    Serial.print(analogRead(timePot));
+    Serial.print(", ");
+    Serial.print(analogRead(speedPot));
+    Serial.print(", ");
+    Serial.println(digitalRead(startButton));
+    delay(100);
 }
